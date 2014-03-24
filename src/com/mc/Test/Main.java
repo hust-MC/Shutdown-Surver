d7199553bed34990e8e0e0c743aeee627b0f70b6
mc <mc_zy@outlook.com>
2014-03-24 16:57:46 +0800

    start message

--- src/com/mc/Test/Main.java
+++ src/com/mc/Test/Main.java
@@ -3,12 +3,15 @@ package com.mc.Test;
 import java.io.*;
 import java.net.*;
 
+import javax.swing.JOptionPane;
+
 public class Main
 {
 	// Choose a port outside of the range 1-1024:
 	public static final int PORT = 6666;
 	public static void main(String[] args) throws IOException
 	{
+		JOptionPane.showMessageDialog(null, "远程关机程序(PC端)启动成功!");
 		ServerSocket s = new ServerSocket(PORT);
 		System.out.println("Started: " + InetAddress.getLocalHost());
 		Socket socket = s.accept();
